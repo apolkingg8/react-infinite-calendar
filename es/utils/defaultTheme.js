9fdfcc165d9380effa2a6b4b0ec45a2c71fe3c97
EijiHsu <apolkingg8@gmail.com>
2017-11-21 17:40:26 +0800

    add build

---
+++ es/utils/defaultTheme.js
@@ -0,0 +1,16 @@
+module.exports = {
+  accentColor: '#8BC34A',
+  floatingNav: {
+    background: 'rgba(66, 66, 66, 0.94)',
+    chevron: '#8BC34A',
+    color: '#FFF'
+  },
+  headerColor: '#8BC34A',
+  selectionColor: '#8BC34A',
+  textColor: {
+    active: '#FFF',
+    default: '#b4b4b4'
+  },
+  todayColor: '#8BC34A',
+  weekdayColor: '#8BC34A'
+};
